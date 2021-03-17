bbc45cb1c9085077f0bf917c5e2b5c3a67216050
GavHensley <gavinrhensley@gmail.com>
2021-03-17 14:32:00 -0400

    finished features 1 and 2

---
+++ src/js/DonutMaker.js
@@ -0,0 +1,27 @@
+class DonutMaker{
+    constructor(){
+        this._donutCount = 0;
+        this._autoClickerCount = 0;
+        this._autoClickerCost = 100;
+
+    }
+
+    getDonutCount(){
+        return this._donutCount;
+    }
+
+    addDonut(){
+        this._donutCount++;
+    }
+
+    getAutoClickerCount(){
+        return this._autoClickerCount;
+    }
+    
+    addAutoClicker(){
+        this._autoClickerCount++;
+        this._donutCount -= this._autoClickerCost;
+    }
+
+    
+}
