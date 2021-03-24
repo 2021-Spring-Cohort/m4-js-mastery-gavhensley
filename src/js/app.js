6dad73573544901a09571a567313c989c8556402
Gav Hensley <gavinrhensley@gmail.com>
2021-03-23 21:10:21 -0400

    iteration 3 feature 3 complete

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 var myShop = new DonutMaker();
 
-let counter = document.getElementById('DonutCounter');
-counter.innerText = "Donut count: " + myShop._donutCount;
+//counters and buttons
+let donutCounter = document.getElementById('DonutCounter');
+donutCounter.innerText = "Donut count: " + myShop._donutCount;
+
+let autoBakerCost = document.getElementById('ClickerCost');
+autoBakerCost.innerText = "Current AutoBaker\u2122 cost: " + myShop._autoClickerCost + " donuts." ;
+
+let autoBakerCounter = document.getElementById('ClickerCount');
+autoBakerCounter.innerText = "AutoBakers\u2122 owned: " + myShop._autoClickerCount;
+
+
 
